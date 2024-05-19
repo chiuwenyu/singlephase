5de43e69858f8cb8bd2eb143257f51d3bec2f419
WENYU CHIU <chiuwenyu@gmail.com>
2024-05-19 15:34:36 +0800

    implement velocity fn

--- src/signle_phase.rs
+++ src/signle_phase.rs
@@ -1,12 +1,52 @@
 
 pub mod single_phase_line {
+    use std::f64::consts::PI;
+
     pub struct SingleFx {
-        pub rho: f64,       // liquid density [kg/m^3]
-        pub mu: f64,        // liquid viscosity [N-sec/m^2] = [Kg/m3/sec]
-        pub id: f64,        // pipe inside diameter [m]
-        pub w: f64,         // liquid flow rate [kg/hr]
-        pub e: f64,         // pipe roughness [m]
-        pub fdarcy: f64,        // Darcy Friction Factor [-]
-        pub sf: f64,        // safety factor [-]
+        // input fields
+        w: f64,         // fluid flow rate [kg/hr]
+        rho: f64,       // fluid density [kg/m^3]
+        mu: f64,        // fluid viscosity [N-sec/m^2] = [Kg/m3/sec]
+        id: f64,        // pipe inside diameter [m]
+        e: f64,         // pipe roughness [m]
+        sf: f64,        // safety factor [-]
+
+        // output fields
+        pub v: f64,         // fluid flow velocity [m/s]
+        pub nre: f64,       // Reynold number [-]
+        pub fdarcy: f64,    // Darcy Friction Factor [-]
+        pub dp100: f64,     // pressure drop per 100m
+        pub vh: f64,        // velocity head vh = rho * v^2
+    }
+
+    impl SingleFx {
+        pub fn new(w: f64, rho: f64, mu: f64, id: f64, e: f64, sf: f64) -> SingleFx {
+            SingleFx {
+                w,
+                rho,
+                mu,
+                id,
+                e,
+                sf,
+                v: -999.0,
+                nre: -999.0,
+                fdarcy: -999.0,
+                dp100: -999.0,
+                vh: -999.0
+            }
+        }
+
+        pub fn velocity(&mut self) -> Result<f64, &'static str> {
+            let ret: Result<f64, &'static str>;
+            if (self.rho * self.id != 0.0) {
+                self.v = self.w / self.rho / (PI / 4.0 * self.id * self.id ) / 3600.0;
+                ret = Ok(self.v);
+            }
+            else {
+                ret = Err("ZeroDivisionError: division by zero caused rho or id");
+            }
+            return ret;
+        }
+
     }
 }
